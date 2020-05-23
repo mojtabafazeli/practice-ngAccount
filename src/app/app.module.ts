90313a237b7c2a822c32d34ac34ea9f2c9c62e3a
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-05-23 13:26:17 -0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [ BrowserModule, FormsModule ],
+	declarations: [ AppComponent, AccountComponent, NewAccountComponent ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
